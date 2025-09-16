725adf77c44d00492a4ed1913c4bf865be3a47d3
gfarlz <ghifariaku@gmail.com>
2025-09-16 15:59:34 +0800

    Donasi.java

--- model/Donasi.java
+++ model/Donasi.java
@@ -1,43 +1,15 @@
 package model;
 
 public class Donasi {
-    private int id;
-    private String namaDonatur;
-    private double jumlah;
-    private String penerima;
+    public int id;
+    public String namaDonatur;
+    public double jumlah;
+    public String penerima;
 
     public Donasi(int id, String namaDonatur, double jumlah, String penerima) {
         this.id = id;
         this.namaDonatur = namaDonatur;
         this.jumlah = jumlah;
-        this.penerima = penerima;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNamaDonatur() {
-        return namaDonatur;
-    }
-
-    public void setNamaDonatur(String namaDonatur) {
-        this.namaDonatur = namaDonatur;
-    }
-
-    public double getJumlah() {
-        return jumlah;
-    }
-
-    public void setJumlah(double jumlah) {
-        this.jumlah = jumlah;
-    }
-
-    public String getPenerima() {
-        return penerima;
-    }
-
-    public void setPenerima(String penerima) {
         this.penerima = penerima;
     }
 
